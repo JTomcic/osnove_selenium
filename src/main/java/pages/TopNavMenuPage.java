003d81e17db462a587216fea8bf3155e472dc377
Jovana Tomcic <tomcicjovana92@gmail.com>
2023-10-13 18:05:31 +0200

    Add SwagLab pages files

---
+++ src/main/java/pages/TopNavMenuPage.java
@@ -0,0 +1,22 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class TopNavMenuPage extends BasicPage{
+    public TopNavMenuPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public WebElement getMenuButton() {
+        return driver.findElement(By.id("react-burger-menu-btn"));
+    }
+
+    public void clickOnMenuButton() {
+        getMenuButton().click();
+    }
+    public boolean doesLogoutButtonExist() {
+        return elementExist(By.linkText("Logout"), 0);
+    }
+
+}
